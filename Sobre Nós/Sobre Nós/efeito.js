7a54b1464262a16cf4cbc64a770ba614de27f584
millenavn <millenav.nunes08@gmail.com>
2024-09-18 00:51:51 -0300

    primeiro commit

---
+++ Sobre Nós/Sobre Nós/efeito.js
@@ -0,0 +1,14 @@
+var btn = document.querySelector(".botao");
+
+btn.onmousemove = function(e){
+    var x = e.pageX - btn.offsetLeft;
+    var y = e.pageY - btn.offsetTop;
+
+btn.style.setProperty('--eixoX', x + 'px')
+btn.style.setProperty('--eixoY', y + 'px')
+}
+
+const myObserver = new IntersectionObserver( (entries) =>{
+console.log(entries)
+
+})
